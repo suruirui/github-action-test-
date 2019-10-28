6ede7826161517413f45bf98fa869b3ea64b3383
suruirui <guimei201314@gmail.com>
2019-10-28 11:23:52 +0800

    button
    button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import Button from "./components/Button";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Test github action.</p>
+        <Button></Button>
         <p>Test github action.</p>
         <p>Test github action.</p>
       </header>
---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./style.css";
+class Button extends Component {
+  constructor() {}
+
+  render() {
+    return <div className="btn">点击我</div>;
+  }
+}
+
+export default Button;
