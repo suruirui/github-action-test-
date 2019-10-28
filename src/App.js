d2599102a6d014c2cc773a3b1eaaddef882ecc3d
suruirui <guimei201314@gmail.com>
2019-10-28 11:29:41 +0800

    btn modify
    btn modify

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Button></Button>
-        <p>Test github action.</p>
-        <p>Test github action.</p>
+        <p>Test github action successfully~!</p>
       </header>
     </div>
   );
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import "./style.css";
 class Button extends Component {
-  constructor() {}
-
   render() {
-    return <div className="btn">点击我</div>;
+    return <div className="btn">Action now</div>;
   }
 }
 
