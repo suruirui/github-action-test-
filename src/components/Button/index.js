8a0de8f56fce4881777f2111bb7506d811fa503f
suruirui <guimei201314@gmail.com>
2019-10-28 11:55:22 +0800

    btn click
    btn click

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import "./style.css";
 class Button extends Component {
+  handleBtn = () => {
+    alert(this.props.msg);
+  };
   render() {
-    return <div className="btn">{this.props.msg}</div>;
+    return (
+      <div className="btn" onClick={this.handleBtn}>
+        {this.props.msg}
+      </div>
+    );
   }
 }
 
