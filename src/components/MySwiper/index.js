e35d00782d2d450d3aa45073c15a599c008b96da
suruirui <guimei201314@gmail.com>
2019-11-10 20:36:54 +0800

    swiper style
    swiper style

--- src/components/MySwiper/index.js
+++ src/components/MySwiper/index.js
@@ -5,7 +5,82 @@ import "./style.css";
 class MySwiper extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      sImgLst: [
+        {
+          src:
+            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3690492852,467272305&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3637245786,1201327908&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1684254966,82635217&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3136075639,3338708347&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2530356297,1854337845&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1514848387,1509766806&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3637245786,1201327908&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4033919967,4068691540&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1685820617,3891463760&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=4129492380,3609907971&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2530356297,1854337845&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1514848387,1509766806&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3637245786,1201327908&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4033919967,4068691540&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1685820617,3891463760&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=4129492380,3609907971&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3637245786,1201327908&fm=26&gp=0.jpg"
+        },
+        {
+          src:
+            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4033919967,4068691540&fm=26&gp=0.jpg"
+        }
+      ]
+    };
   }
 
   componentDidMount = () => {
