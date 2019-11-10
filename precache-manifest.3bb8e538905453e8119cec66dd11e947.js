572065d2178ae26eab514799c4c0ce4295c4af21
suruirui <guimei201314@gmail.com>
2019-11-10 12:38:44 +0000

    Deploying to gh-pages from master e35d00782d2d450d3aa45073c15a599c008b96da

---
+++ precache-manifest.3bb8e538905453e8119cec66dd11e947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c22705249edcb1e634bf5351b1c8fd",
+    "url": "/github-action-test-/index.html"
+  },
+  {
+    "revision": "95003fc75a83cddcd3b1",
+    "url": "/github-action-test-/static/css/2.5d36b48a.chunk.css"
+  },
+  {
+    "revision": "e03a3ce1d15f0fe7bc63",
+    "url": "/github-action-test-/static/css/main.c1f9d89a.chunk.css"
+  },
+  {
+    "revision": "95003fc75a83cddcd3b1",
+    "url": "/github-action-test-/static/js/2.876f2aa8.chunk.js"
+  },
+  {
+    "revision": "e03a3ce1d15f0fe7bc63",
+    "url": "/github-action-test-/static/js/main.b21b69b9.chunk.js"
+  },
+  {
+    "revision": "c441e0d9a8bb45d4e811",
+    "url": "/github-action-test-/static/js/runtime-main.bcb02c2d.js"
+  }
+]);
